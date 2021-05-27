c619a9384b7a4090e6ceb56e87a39c55c083f26d
Ozkan Akkaya <ozkanakkaya@hotmail.com.tr>
2021-05-28 00:37:52 +0300

    8.gün dersi canlı izlendi ve uygulandı

--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -5,37 +5,43 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data//getter ve setterları ekler
+@Data // getter ve setterları ekler
 @Entity
-@Table(name="products")
-@AllArgsConstructor//argümanları kullanarak constructor ekler
-@NoArgsConstructor//parametresiz constructor ekler
+@Table(name = "products")
+@AllArgsConstructor // argümanları kullanarak constructor ekler
+@NoArgsConstructor // parametresiz constructor ekler
 public class Product {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name="product_id")	
+	@Column(name = "product_id")
 	private int id;
-	
-	@Column(name="category_id")
-	private int categoryId;
-	
-	@Column(name="product_name")
-	private String pruductName;
-	
-	@Column(name="unit_price")
+
+//	@Column(name="category_id")
+//	private int categoryId;
+
+	@Column(name = "product_name")
+	private String productName;
+
+	@Column(name = "unit_price")
 	private double unitPrice;
-	
-	@Column(name="units_in_stock")
+
+	@Column(name = "units_in_stock")
 	private short unitsInStock;
-	
-	@Column(name="quantity_per_unit")
+
+	@Column(name = "quantity_per_unit")
 	private String quantityPerUnit;
-	
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
 }
